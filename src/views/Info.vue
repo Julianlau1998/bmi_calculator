6b7efaa933a3f7d8ef998b48f4db39f4c19d12a1
julianlau1998 <Julian.lau@aol.com>
2022-10-13 20:26:07 +0200

    Back button

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,14 +1,25 @@
 <template>
-  <iframe
-      src="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html"
-      frameborder="0"
-      class="iframe"
-  >
-  </iframe>
+  <div>
+    <span @click="back" class="is-back-button">
+      <i @click="back" class="fas fa-arrow-left is-back-icon mr-2"></i>
+      Back
+    </span>
+    <iframe
+        src="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html"
+        frameborder="0"
+        class="iframe"
+    >
+    </iframe>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Info-bmi"
+  name: "Info-bmi",
+  methods: {
+    back () {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
