efd28d197a65a11550710571d00e7977c779bf58
julianlau1998 <Julian.lau@aol.com>
2022-10-13 20:16:57 +0200

    Added cdc as source

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,6 +1,6 @@
 <template>
   <iframe
-      src="https://www.who.int/europe/news-room/fact-sheets/item/a-healthy-lifestyle---who-recommendations#:~:text=BMI%20is%20derived%20from%20a,24.9%20as%20a%20normal%20BMI."
+      src="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html"
       frameborder="0"
       class="iframe"
   >
